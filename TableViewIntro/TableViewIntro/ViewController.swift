cd7d08f2f8f6675af684f069c05791da396757d1
niksolaz <solazzo.nicola@gmail.com>
2017-11-13 17:00:11 +0100

    Added method to create list of books

---
+++ TableViewIntro/TableViewIntro/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  TableViewIntro
+//
+//  Created by Nicola Solazzo on 13/11/17.
+//  Copyright © 2017 Nicola Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var books = ["game of Thrones", "Swift for dummies", "Javascript", "Proggrammatore per Hobby", "Huckleberry Finn", "Tom Sawyer"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Test"
+        return cell
+    }
+    
+    
+}
